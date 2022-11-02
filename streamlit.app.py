6aa3ecb43e8f3bac658aaa3c996133653284b2d3
Raven Hatcher <raven.d.hatcher@gmail.com>
2022-11-02 15:04:30 -0500

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -20,4 +20,4 @@ fruit_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit_list.inde
 fruits_to_show=my_fruit_list.loc[fruit_selected]
 
 #display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruit_to_show)
