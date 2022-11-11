9cda6da0a61d0b366d14615214d24d74fa638258
Raven Hatcher <raven.d.hatcher@gmail.com>
2022-11-11 16:02:42 -0600

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib2.HTTPerror import URLError
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
